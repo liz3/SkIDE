f4b2ba7086c0130ca388895e0884d6dad2feb906
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2018-03-28 12:33:44 +0100

    Update model again

--- src/main/java/com/skide/skriptinsight/model/InspectionResult.java
+++ src/main/java/com/skide/skriptinsight/model/InspectionResult.java
@@ -1,18 +1,18 @@
 package com.skide.skriptinsight.model;
 
-import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class InspectionResult {
+    private String requestID;
     private InspectionResultElement[] inspectionResults;
-    private String allFixedTokens;
+
+    @JsonProperty("RequestId")
+    public String getRequestID() { return requestID; }
+    @JsonProperty("RequestId")
+    public void setRequestID(String value) { this.requestID = value; }
 
     @JsonProperty("InspectionResults")
     public InspectionResultElement[] getInspectionResults() { return inspectionResults; }
     @JsonProperty("InspectionResults")
     public void setInspectionResults(InspectionResultElement[] value) { this.inspectionResults = value; }
-
-    @JsonProperty("AllFixedTokens")
-    public String getAllFixedTokens() { return allFixedTokens; }
-    @JsonProperty("AllFixedTokens")
-    public void setAllFixedTokens(String value) { this.allFixedTokens = value; }
 }
