ca5cda19456bf48cdc48f3b5163a363e25a53dcb
Liz3 <accs@21xayah.com>
2018-12-07 22:30:44 +0100

    Fixed a condition where the Definition Finder would fail

--- src/main/kotlin/com/skide/core/code/DefinitionsFinder.kt
+++ src/main/kotlin/com/skide/core/code/DefinitionsFinder.kt
@@ -5,7 +5,7 @@ import com.skide.include.NodeType
 import com.skide.utils.EditorUtils
 import java.util.*
 
-class DefinitionFinderResult(val success: Boolean, val line: Int = -1, val column: Int = -1, val fName:String = "")
+class DefinitionFinderResult(val success: Boolean, val line: Int = -1, val column: Int = -1, val fName: String = "")
 
 class DefinitionsFinder(val manager: CodeManager) {
 
@@ -32,39 +32,40 @@ class DefinitionsFinder(val manager: CodeManager) {
                 }
             }
 
-            }
+        }
         if (line.nodeType == NodeType.IF_STATEMENT || line.nodeType == NodeType.STATEMENT || line.nodeType == NodeType.LOOP || line.nodeType == NodeType.SET_VAR || line.nodeType == NodeType.IF_STATEMENT) {
             val raw = line.raw
 
             if (raw.indexOf(word) > 1) {
                 if ((raw[raw.indexOf(word) - 1] == '{') || (raw[raw.indexOf(word) - 1] == ':' && raw.contains("{"))) {
-                    if(word.startsWith("_")) {
+                    if (word.startsWith("_")) {
                         val root = EditorUtils.getRootOf(line)
                         EditorUtils.filterByNodeType(NodeType.SET_VAR, root).forEach {
-                            if((it.fields["name"] as String).contains(word)) {
+
+                            if ((it.fields["name"] as String).contains(word.substring(1)) && it.fields["visibility"] == "local") {
 
                                 return DefinitionFinderResult(true, it.linenumber, it.raw.indexOf(word) + 1)
                             }
                         }
-                        if(root.nodeType == NodeType.FUNCTION) {
+                        if (root.nodeType == NodeType.FUNCTION) {
                             val params = root.fields["params"] as Vector<MethodParameter>
                             params.forEach {
-                                if(it.name == word.substring(1)) {
+                                if (it.name == word.substring(1)) {
                                     return DefinitionFinderResult(true, root.linenumber, root.raw.indexOf(word.substring(1)) + 1)
                                 }
                             }
                         }
                     } else {
                         EditorUtils.filterByNodeType(NodeType.SET_VAR, nodeStructure).forEach {
-                            if((it.fields["name"] as String).contains(word)) {
+                            if ((it.fields["name"] as String).contains(word)) {
 
                                 return DefinitionFinderResult(true, it.linenumber, it.raw.indexOf(word) + 1)
                             }
                         }
                     }
-                } else if(raw[raw.indexOf(word) + word.length] == '(') {
+                } else if (raw[raw.indexOf(word) + word.length] == '(') {
                     EditorUtils.filterByNodeType(NodeType.FUNCTION, nodeStructure).forEach {
-                        if(it.fields["name"] == word) {
+                        if (it.fields["name"] == word) {
 
                             return DefinitionFinderResult(true, it.linenumber, 1)
                         }
