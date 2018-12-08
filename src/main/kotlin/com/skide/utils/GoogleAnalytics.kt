8209a5c2c3783f79b9de7915b211bc8b0e2dd64f
Liz3 <accs@21xayah.com>
2018-12-08 17:49:24 +0100

    fixed a bug that would prevent startup

--- src/main/kotlin/com/skide/utils/GoogleAnalytics.kt
+++ src/main/kotlin/com/skide/utils/GoogleAnalytics.kt
@@ -4,7 +4,7 @@ import com.brsanthu.googleanalytics.GoogleAnalytics
 import com.skide.CoreManager
 import com.skide.gui.GUIManager
 
-class GoogleAnalytics(val coreManager:CoreManager) {
+class GoogleAnalytics(val coreManager: CoreManager) {
 
     var enabled = false
     lateinit var ga: GoogleAnalytics
@@ -17,19 +17,26 @@ class GoogleAnalytics(val coreManager:CoreManager) {
     fun start() {
         enabled = true
         ga = GoogleAnalytics.builder().withTrackingId("UA-130243026-3").build()
-        val startR = ga.screenView()
-                .sessionControl("start")
-                .send().requestParams.get("cid")!!
-        ga.pageView()
-                .documentTitle("Sk-IDE main menu")
-                .documentPath("/start")
-                .send()
+        try {
+            ga.screenView()
+                    .sessionControl("start")
+                    .send().requestParams.get("cid")!!
+        } catch (e: Exception) {}
+        try {
+            ga.pageView()
+                    .documentTitle("Sk-IDE main menu")
+                    .documentPath("/start")
+                    .send()
+        } catch (e: Exception) { }
+
         GUIManager.closingHooks.add {
             println("Sending session close")
-            ga.pageView()
-                    .documentTitle("stopped")
-                    .documentPath("/end")
-                    .send()
+            try {
+                ga.pageView()
+                        .documentTitle("stopped")
+                        .documentPath("/end")
+                        .send()
+            }catch (e:Exception) { }
         }
     }
 
