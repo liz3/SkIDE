a1bf553581947fdee5993d82d59d6aaf98b5c5ec
Liz3 <accs@21xayah.com>
2019-01-04 16:59:14 +0100

    updated version string

--- src/main/kotlin/com/skide/Main.kt
+++ src/main/kotlin/com/skide/Main.kt
@@ -1,8 +1,6 @@
 package com.skide
 object Info {
-    const val version = "2019.1u4"
+    const val version = "2019.1u5"
     var prodMode = false
-    var serverPort = 0
-
 }
 fun main(args: Array<String>) = CoreManager().bootstrap(args)
